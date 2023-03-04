490cffa2332167002a41a7a1751c09a81b77046c
Oscar Escobar <you@example.com>
2023-03-04 11:10:45 -0600

    agregar

--- Ejercicio Class 1 (copia)/ejemplo1.c
+++ Ejercicio Class 1 (copia)/ejemplo1.c
@@ -11,56 +11,66 @@ char * names[]={NULL, "db_type","db_name", "db_table_prefix", "db_port"};
 int main(void){
     int ntoken,vtoken;
     ntoken= yylex();
-    while (ntoken)
-    {
-        printf("%d \n", ntoken);
-    
+
+        if(ntoken != TYPE){
+            printf("Error de sintaxis, se esperaba db_type pero no funciono %s\n", yytext);
+            return 1;
+        } 
+        ntoken= yylex();
         if(ntoken != COLON){
             printf("Error de sintaxis, se esperaba : Pero no se encontro %s\n", yytext);
             return 1;
         }   
+        ntoken= yylex();
         if(ntoken != IDENTIFIER){
         printf("Error de sintaxis, se esperaba iDENTIficador %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
          if(ntoken != NAME){
         printf("Error de sintaxis, se esperaba un db_name pero no se encontro %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
          if(ntoken != COLON){
         printf("Error de sintaxis, se esperaba : pero no se encontro %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
          if(ntoken != IDENTIFIER){
         printf("Error de sintaxis, se esperaba iDENTIficador %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
          if(ntoken != TABLE_PREFIX){
         printf("Error de sintaxis, se esperaba un table preflix %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
          if(ntoken != COLON){
         printf("Error de sintaxis, se sespera : pero no se encontro %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
          if(ntoken != IDENTIFIER){
         printf("Error de sintaxis, se sespera un identificador  %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
         if(ntoken != PORT){
         printf("Error de sintaxis, se esperaba un puerto pero no se encontro  %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
         if(ntoken != COLON){
         printf("Error de sintaxis, se sespera : pero no se encontro %s\n", yytext);
         return 1;
         }
+        ntoken= yylex();
         if(ntoken != INTEGER){
         printf("Error de sintaxis, se esperaba un numero entero pero no se encontro %s\n", yytext);
         return 1;
         }
-        printf("No existen errores de sintaxis");
+        printf("No existen errores de sintaxis \n");
         return 0;
-    }
-    return 0;
 }
